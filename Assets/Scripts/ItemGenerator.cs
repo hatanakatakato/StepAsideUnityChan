e4a6c1f6718f7108caa62b617b055001bbd38739
hatanakatakato <80793900+hatanakatakato@users.noreply.github.com>
2021-08-22 20:47:26 +0900

    発展課題完了
    アイテムを前方60に生成
    後ろに言ったら破棄

--- Assets/Scripts/ItemGenerator.cs
+++ Assets/Scripts/ItemGenerator.cs
@@ -16,13 +16,26 @@ public class ItemGenerator : MonoBehaviour
     private int goalPos = 360;
     //アイテムを出すx方向の範囲
     private float posRange = 3.4f;
+    //unitychan(gameobject)取得
+    private GameObject unitychan;
+    //アイテム出現z座標
+    private float nextItemAppearPosZ = 60;
 
     void Start()
     {
-        //一定の距離ごとにアイテムを生成
-        for (int i = startPos; i < goalPos; i += 15)
+        //同じシーン内のgameobj取得
+        this.unitychan = GameObject.Find("unitychan");
+
+        
+    }
+
+    void Update()
+    {
+        //z軸60前方にアイテムを出現させ続ける(15区切りで）(goalまで）
+        if (this.nextItemAppearPosZ - this.unitychan.GetComponent<Transform>().transform.position.z < 60)
         {
-            //どのアイテムを出すのかをランダムにする
+            
+            //coneを出すかその他かランダムに決める
             int num = Random.Range(1, 11);
 
             if (num <= 2)
@@ -34,7 +47,7 @@ public class ItemGenerator : MonoBehaviour
                     //conePrefabのインスタンスを生成
                     GameObject cone = Instantiate(conePrefab);
                     //インスタンスを配置
-                    cone.transform.position = new Vector3(4 * j, cone.transform.position.y, i);
+                    cone.transform.position = new Vector3(4 * j, cone.transform.position.y, this.nextItemAppearPosZ);
                 }
             }
             else
@@ -42,7 +55,7 @@ public class ItemGenerator : MonoBehaviour
                 //レーンごとにアイテムを生成
                 for (int j = -1; j <= 1; j++)
                 {
-                    //アイテムの種類を決める
+                    //cone以外でアイテムの種類を決める
                     int item = Random.Range(1, 11);
                     //アイテムを置くZ座標のオフセットをランダムに設定
                     int offsetZ = Random.Range(-5, 6);
@@ -52,22 +65,21 @@ public class ItemGenerator : MonoBehaviour
                         //coinPrefabをインスタンス化
                         GameObject coin = Instantiate(coinPrefab);
                         //インスタンスを配置
-                        coin.transform.position = new Vector3(posRange * j, coin.transform.position.y, i + offsetZ);
+                        coin.transform.position = new Vector3(posRange * j, coin.transform.position.y, this.nextItemAppearPosZ + offsetZ);
                     }
                     else if (7 <= item && item <= 9)
                     {
                         //carPrefabをインスタンス化
                         GameObject car = Instantiate(carPrefab);
                         //インスタンスを配置
-                        car.transform.position = new Vector3(posRange * j, car.transform.position.y, i + offsetZ);
+                        car.transform.position = new Vector3(posRange * j, car.transform.position.y, this.nextItemAppearPosZ + offsetZ);
                     }
                 }
             }
+           
+            //次のアイテム出現位置を決定
+            this.nextItemAppearPosZ += 15;
+
         }
-    }
-
-    void Update()
-    {
-
     }
 }
